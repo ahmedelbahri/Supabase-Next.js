30feae518b3c96f98d82bc8302f88ccfcd853e8f
Ahmed El bahri <ahel-bah@e1r3p10.1337.ma>
2024-01-07 01:30:56 +0100

    auth ui

--- app/(app)/(public)/login/page.tsx
+++ app/(app)/(public)/login/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient(
+	process.env.NEXT_PUBLIC_SUPABASE_URL!,
+	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
+
+	
 export default async function Page() {
-  return null;
-}
+	return (
+	<>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='h-fit min-w-[270px]'>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          theme='dark'
+          providers={[]}
+        />
+      </div>
+    </div>
+	</>
+	);
+  }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default async function Page() {
   return (
-    <div>this is a dashboard</div>
+    <span className="flex justify-center items-center">this is a dashboard</span>
   );
 }
